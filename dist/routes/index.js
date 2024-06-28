0df1ef66445b15b8bf73bf3b5c3f518b9744aa48
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-06-28 12:33:25 -0500

    Corrección ruta start

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -11,7 +11,7 @@ var _routesAdmin = _interopRequireDefault(require("./routes.admin.js"));
 var _routesBarbero = _interopRequireDefault(require("./routes.barbero.js"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
 var ruta = (0, _express.Router)();
-ruta.use("/home", _routesHome["default"]);
+ruta.use("/", _routesHome["default"]);
 ruta.use("/cliente", _routesClientes["default"]);
 ruta.use("/admin", _routesAdmin["default"]);
 ruta.use("/barbero", _routesBarbero["default"]);
--- dist/routes/routes.home.js
+++ dist/routes/routes.home.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 var _express = require("express");
 var _controllersHome = require("../controllers/controllers.home.js");
 var rutaHome = (0, _express.Router)();
-rutaHome.get("/login", _controllersHome.login);
+rutaHome.get("/", _controllersHome.login);
 rutaHome.get("/registro", _controllersHome.registro);
 rutaHome.get("/contrasena", _controllersHome.contrasena);
 var _default = exports["default"] = rutaHome;
