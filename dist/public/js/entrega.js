fd139d22f5321e9d2330a8e013eda3d26250d092
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-06-30 18:35:05 -0500

    Metodo buscar en página principal arreglado

--- dist/public/js/entrega.js
+++ dist/public/js/entrega.js
@@ -1,29 +1,7 @@
-function seleccionarPregunta(id) {
-    localStorage.setItem('preguntaSeleccionada', id);
-}
-function redireccionarEditar() {
-    const id = localStorage.getItem('preguntaSeleccionada');
-
-    if (id) {
-        window.location.href = `/admin/pregunta/editar?id=${id}`;
-    } else {
-        Swal.fire({
-            icon: 'warning',
-            title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Seleccione una pregunta primero' + "</h5>",
-            showConfirmButton: false,
-            timer: 1500,
-            customClass: {
-                popup: 'bg-alert',
-                content: 'text-alert'
-            }
-        });
-    }
-}
-
-async function eliminarPregunta() {
-    const id = localStorage.getItem('preguntaSeleccionada');
+async function eliminarEntrega(id) {
+    localStorage.getItem('entregaSeleccionada', id);
     Swal.fire({
-        title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + '¿Estás seguro de eliminarlo?' + "</h5>",
+        title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + '¿Estás seguro de marcar como entregada?' + "</h5>",
         icon: "warning",
         showCancelButton: true,
         confirmButtonColor: "#318331",
@@ -36,14 +14,12 @@ async function eliminarPregunta() {
         }
     }).then(async (result) => {
         if (result.isConfirmed) {
-            const token = sessionStorage.getItem("token")
-            const urlLogic = sessionStorage.getItem("urlLogic") + "/preguntas/desactivar";
-            if (id) { 
+            const urlLogic = sessionStorage.getItem("urlLogic") + "/ventas/desactivar";
+            if (id) {
                 const respuesta = await fetch(urlLogic, {
                     method: 'POST',
                     headers: {
-                        'Content-Type': 'application/json',
-                        "x-access-token": token
+                        'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({ id: id })
                 });
@@ -51,7 +27,7 @@ async function eliminarPregunta() {
                 if (respuesta.ok) {
                     Swal.fire({
                         icon: 'success',
-                        title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Pregunta eliminada exitosamente' + "</h5>",
+                        title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Entrega marcada exitosamente' + "</h5>",
                         showConfirmButton: false,
                         timer: 1500,
                         customClass: {
@@ -76,7 +52,7 @@ async function eliminarPregunta() {
             } else {
                 Swal.fire({
                     icon: 'error',
-                    title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Seleccione una pregunta primero' + "</h5>",
+                    title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Seleccione una entrega primero' + "</h5>",
                     showConfirmButton: false,
                     timer: 1500,
                     customClass: {
