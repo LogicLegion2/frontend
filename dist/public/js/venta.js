5813f3088a4d3c6e5e1edc851fbe8e8950b397ef
Paulina200R <paulinaramirezmorales12@gmail.com>
2024-07-06 14:16:08 -0500

    Corrección de guardar productos y eliminar del carrito

--- dist/public/js/venta.js
+++ dist/public/js/venta.js
@@ -4,49 +4,60 @@ document.getElementById('form-comprar-producto').addEventListener('submit', asyn
     const metodoPago = document.getElementById('metodoPago').value;
     const metodoEntrega = document.getElementById('metodoEntrega').value;
     const direccion = document.getElementById('direccion').value;
-    const checkout = document.getElementById('checkout');
 
     const productos = JSON.parse(localStorage.getItem('productos') || '[]');
     const totalGlobal = localStorage.getItem('totalGlobal');
     const id = sessionStorage.getItem('id');
     const urlLogic = sessionStorage.getItem("urlLogic") + "/ventas/compra";
+    const recursoPago = sessionStorage.getItem("urlLogic") + "/create-order"
     const token = sessionStorage.getItem('token');
 
     try {
-        const response = await fetch(urlLogic, {
+        const responsePago = await fetch(recursoPago, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
-                "x-access-token": token
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ id, productos, totalGlobal, metodoPago, metodoEntrega, direccion })
+            body: JSON.stringify({ totalGlobal })
         });
 
-        const recursoPago = sessionStorage.getItem("urlLogic") + "/create-order"
-        const responsePago = await fetch(recursoPago, {
-            method: 'POST'
-        });
         const data = await responsePago.json();
-        // console.log({ "PAGO ": recursoPago, "DATA": data });
         window.location.href = data.links[1].href;
 
-        if (response.ok && responsePago.ok) {
-            // localStorage.removeItem('productos');
-            // localStorage.removeItem('totalGlobal');
+        if (responsePago.ok) {
+            const metodoPago = document.getElementById('metodoPago').value;
+            const metodoEntrega = document.getElementById('metodoEntrega').value;
+            const direccion = document.getElementById('direccion').value;
 
-            // Swal.fire({
-            //     icon: 'success',
-            //     title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Compra realizada exitosamente' + "</h5>",
-            //     showConfirmButton: false,
-            //     timer: 1500,
-            //     customClass: {
-            //         popup: 'bg-alert',
-            //         content: 'text-alert'
-            //     }
-            // });
-            // setTimeout(() => {
-            //     window.location.href = `/cliente/menu`;
-            // }, 1500);
+            const productos = JSON.parse(localStorage.getItem('productos') || '[]');
+            const totalGlobal = localStorage.getItem('totalGlobal');
+            const token = sessionStorage.getItem('token');
+
+            const response = await fetch(urlLogic, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "x-access-token": token
+                },
+                body: JSON.stringify({ id, productos, totalGlobal, metodoPago, metodoEntrega, direccion })
+            });
+            if (response.ok) {
+                localStorage.removeItem('productos');
+                localStorage.removeItem('totalGlobal');
+            } else {
+                Swal.fire({
+                    icon: 'warning',
+                    title: "<h5 style='color:white; font-family: 'Aleo', serif;'>" + 'Error PayPal' + "</h5>",
+                    showConfirmButton: false,
+                    timer: 1500,
+                    customClass: {
+                        popup: 'bg-alert',
+                    }
+                });
+                setTimeout(() => {
+                    window.location.href = `/cliente/carrito`;
+                }, 1500);
+            }
         } else {
             Swal.fire({
                 icon: 'warning',
